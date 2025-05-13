bfad1eb9b2a6bed1535b5c9321c95a7fc1f9468e
IoanaxX7 <ioanaxx7@gmail.com>
2025-05-13 21:43:36 +0300

    login

--- login.php
+++ login.php
@@ -36,6 +36,9 @@ file_exists(__DIR__ . "/config.php") ?
                         <label for="password">Parola:</label>
                         <input type="password" id="parola" name="parola" required>
                     </div>
+                    <div class="forgot-password">
+                        <a href="#">Ai uitat parola?</a>
+                    </div>
                     <button class="form-btn">Log in</button>
                 </form>
             </div>
