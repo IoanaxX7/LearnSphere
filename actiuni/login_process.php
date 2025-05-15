6960181764dca58af685304ea42c9c5bfd382523
IoanaxX7 <ioanaxx7@gmail.com>
2025-05-15 17:41:53 +0300

    login_process

--- actiuni/login_process.php
+++ actiuni/login_process.php
@@ -12,6 +12,7 @@ if (isset($_POST['nume_utilizator']) && isset($_POST['parola'])) {
 
     if ($user && password_verify($password, $user['parola'])) {
         $_SESSION['username'] = $username;
+        $_SESSION['rol'] = $user['rol'];
         header("Location: ../index.php");
         exit();
     } else {
