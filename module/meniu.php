942113e7cde2261014c8cb2b401b4183e48423f3
IoanaxX7 <ioanaxx7@gmail.com>
2025-07-02 22:31:49 +0300

    meniu

--- module/meniu.php
+++ module/meniu.php
@@ -27,6 +27,7 @@
                         <li><a class="dropdown-item" href="<?php echo url() ?>/postari/form_intrebare.php">Pune o întrebare</a></li>
                     </ul>
                 </li>
+
                 <?php if (in_array($_SESSION["rol"], [1])): ?>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="adminDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -44,23 +45,39 @@
 
             <div class="d-flex flex-grow-1 justify-content-center my-2 my-lg-0">
                 <form class="input-group w-50" role="search">
-                    <span class="input-group-text bg-white">
+                    <input type="search" class="form-control" placeholder="Search" aria-label="Caută">
+                    <button class="btn meniuBtn" type="submit">
                         <i class="bi bi-search"></i>
-                    </span>
-                    <input type="search" class="form-control" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-light" type="submit">Search</button>
+                    </button>
                 </form>
             </div>
 
-            <div class="dropdown ms-3 mt-2 mt-lg-0">
-                <a href="#" class="btn p-0 border-0 bg-transparent" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="<?php echo url() . '/uploads/' . $_SESSION['username'] . '/' . $_SESSION['pozaProfil']; ?>" alt="Profile" class="rounded-circle" style="width: 30px; height: 30px;">
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
-                    <li><a class="dropdown-item" href="<?php echo url() ?>/setari/setari.php">Setări</a></li>
-                    <li><a class="dropdown-item" href="<?php echo url() ?>/logout.php">Deconectare</a></li>
-                </ul>
-            </div>
+            <?php
+            if (!empty($_SESSION["username"]) || in_array($_SESSION["rol"], $Roluri)) {
+                $imgSrc = url() . '/uploads/' . $_SESSION['username'] . '/' . $_SESSION['pozaProfil'];
+                $setariUrl = url() . '/setari/setari.php';
+                $logoutUrl = url() . '/logout.php';
+
+                echo '<div class="dropdown ms-3 mt-2 mt-lg-0">
+                    <a href="#" class="btn p-0 border-0 bg-transparent" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="' . htmlspecialchars($imgSrc) . '" alt="Profile" class="rounded-circle" style="width: 30px; height: 30px;">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
+                        <li><a class="dropdown-item" href="' . htmlspecialchars($setariUrl) . '">Setări</a></li>
+                        <li><a class="dropdown-item" href="' . htmlspecialchars($logoutUrl) . '">Deconectare</a></li>
+                    </ul>
+                </div>';
+            } else {
+                $loginUrl = url() . '/login.php';
+                $signupUrl = url() . '/signin.php';
+
+                echo '<div class="ms-3 mt-2 mt-lg-0 d-flex gap-2">
+                    <a href="' . htmlspecialchars($loginUrl) . '" style="background-color: white; color: #438bab;" class="btn fw-semibold autentificare">Autentificare</a>
+                    <a href="' . htmlspecialchars($signupUrl) . '" style="background-color: #4CAF50; color: white;" class="btn fw-semibold inregistrare">Înregistrare</a>
+                </div>';
+            }
+
+            ?>
         </div>
     </div>
 </nav>
