10d01bb43eace235f93166bb5d4f7f43cea001f7
IoanaxX7 <ioanaxx7@gmail.com>
2025-07-02 22:42:02 +0300

    signup

---
+++ assets/signup.js
@@ -0,0 +1,6 @@
+function showLoginModal() {
+  document.getElementById("loginModal").style.display = "flex";
+}
+function closeLoginModal() {
+  document.getElementById("loginModal").style.display = "none";
+}
