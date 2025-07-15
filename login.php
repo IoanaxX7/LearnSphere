1f1cecbd039825854c9bb605f619dc4f966bb209
IoanaxX7 <ioanaxx7@gmail.com>
2025-07-15 21:58:59 +0300

    login

--- login.php
+++ login.php
@@ -33,9 +33,22 @@ file_exists(__DIR__ . "/config.php") ?
                         <input type="username" id="nume_utilizator" name="nume_utilizator" required>
                     </div>
                     <div class="inputbox">
-                        <label for="password">Parola:</label>
-                        <input type="password" id="parola" name="parola" required>
+                        <div class="input-group" style="display: flex; align-items: center; width: 100%; position: relative;">
+                            <label for="parola" style="margin-right: 10px; white-space: nowrap;">Parola:</label>
+
+                            <div style="position: relative; flex: 1;">
+                                <input type="password" id="parola" name="parola" required style="width: 100%; padding-right: 35px;">
+
+                                <button type="button" class="toggle-password"
+                                    aria-controls="parola" aria-label="Afișează parola"
+                                    style="position: absolute; top: 50%; right: 5px; transform: translateY(-50%); border: none; background: none; padding: 0; cursor: pointer;">
+                                    <i class="bi bi-eye-slash" style="font-size: 1.2em; color: var(--txt);"></i>
+                                </button>
+                            </div>
+                        </div>
                     </div>
+
+
                     <div class="forgot-password">
                         <a href="password_reset.php">Ai uitat parola?</a>
                     </div>
@@ -44,6 +57,8 @@ file_exists(__DIR__ . "/config.php") ?
             </div>
         </div>
     </section>
+
+    <script type="text/javascript" src="assets/show_hide_passwords.js"></script>
 </body>
 
 </html>
